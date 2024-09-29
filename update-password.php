ac6b5bf4bd03f5496b63852ee18f5a57de5dc515
“s-wardah” <saniaekarobiah@gmail.com>
2024-09-29 17:49:59 +0700

    menambahkan fitur ganti password user

--- update-password.php
+++ update-password.php
@@ -23,24 +23,9 @@ include_once('templates/header.php');
             <div class="alert alert-danger" role="alert">
                 Data gagal disimpan!
             </div>
-        <?php
-        }
-    } else if (isset($_POST['ganti_password'])) {
-        if (ganti_password($_POST) > 0) {
-        ?>
-            <div class="alert alert-success" role="alert">
-                Password berhasil diubah!
-            </div>
-        <?php
-        } else {
-        ?>
-            <div class="alert alert-danger" role="alert">
-                PAssword gagal diubah!
-            </div>
     <?php
         }
     }
-
     ?>
     <!-- DataTales Example -->
     <div class="card shadow mb-4">
@@ -112,50 +97,6 @@ $huruf = "usr";
 $kodeuser = $huruf . sprintf('%02s', $urutan);
 ?>
 
-<!-- Modal -->
-<div class="modal fade" id="tambahModal" tabindex="-1" aria-labelledby="tambahModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="tambahModalLabel">Modal title</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <form method="post" action="">
-                    <input type="hidden" name="id_user" id="id_user" value="<?= $kodeuser ?>">
-                    <div class="form-group row">
-                        <label for="username" class="col-sm-3 col-form-label">Username</label>
-                        <div class="col-sm-8">
-                            <input type="text" id="username" class="form-control" name="username">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label for="password" class="col-sm-3 col-form-label">Password</label>
-                        <div class="col-sm-8">
-                            <input type="password" id="password" class="form-control" name="password">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label for="user_role" class="col-sm-3 col-form-label">User Role</label>
-                        <div class="col-sm-8">
-                            <select class="form-control" id="user_role" name="user_role">
-                                <option value="admin">Administrator</option>
-                                <option value="operator">Operator</option>
-                            </select>
-                        </div>
-                    </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Keluar</button>
-                <button type="submit" name="simpan" class="btn btn-primary">Simpan</button>
-            </div>
-            </form>
-        </div>
-    </div>
-</div>
-
 <!-- Modal GAnti Password-->
 <div class="modal fade" id="gantiPassword" tabindex="-1" aria-labelledby="gantiPasswordLabel" aria-hidden="true">
     <div class="modal-dialog">
@@ -174,7 +115,7 @@ $kodeuser = $huruf . sprintf('%02s', $urutan);
                         <div class="col-sm-7">
                             <input type="password" id="password" class="form-control" name="password">
                         </div>
-                    </div>
+                    </div>     
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Keluar</button>
@@ -184,6 +125,7 @@ $kodeuser = $huruf . sprintf('%02s', $urutan);
         </div>
     </div>
 </div>
+
 <!-- /.container-fluid -->
 
 <?php
