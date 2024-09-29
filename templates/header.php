c6b0208104b0038eeb1e6d42cb4f2ce4c2dbf79f
“s-wardah” <saniaekarobiah@gmail.com>
2024-09-29 20:03:54 +0700

    memperbaiki dashboard

--- templates/header.php
+++ templates/header.php
@@ -65,7 +65,7 @@ if (!isset($_SESSION['login'])) {
             <?php endif; ?>
             <!-- Nav Item - Dashboard -->
             <li class="nav-item">
-                <a class="nav-link" href="index.html">
+                <a class="nav-link" href="index.php">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
             </li>
